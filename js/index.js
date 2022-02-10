2f54563560922cd8071f6f7a9174450bcb97b689
manvith <iib2019014@iiita.ac.in>
2022-02-10 10:21:48 +0530

    z-index fit

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 toggler = document.querySelector(".toggler");
-sidenav = document.querySelector(".sidenav");
+navbar = document.querySelector(".navbar");
 content = document.querySelector(".content");
 
-sidenav_status = true;
+navbar_status = true;
 
 toggler.addEventListener("click", function(event) {
-    if(sidenav_status) {
-        sidenav.classList.add("sidenav-collapse");
+    if(navbar_status) {
+        navbar.classList.add("navbar-collapse");
         content.classList.add("content-adjust");
-        sidenav.classList.remove("sidenav-un-collapse");
+        navbar.classList.remove("navbar-un-collapse");
         content.classList.remove("content-un-adjust");
 
-        sidenav_status = false;
+        navbar_status = false;
     }
 
     else {
-        sidenav.classList.add("sidenav-un-collapse");
+        navbar.classList.add("navbar-un-collapse");
         content.classList.add("content-un-adjust");
-        sidenav.classList.remove("sidenav-collapse");
+        navbar.classList.remove("navbar-collapse");
         content.classList.remove("content-adjust");
 
-        sidenav_status = true;
+        navbar_status = true;
     }
 
 })
@@ -33,7 +33,7 @@ active_page = active_page_util[active_page_util.length - 1].classList[1]
 console.log(active_page);
 active_nav_item = document.getElementById(active_page)
 console.log(active_nav_item);
-active_nav_item.classList.add("sidenav-item-active")
+active_nav_item.classList.add("nav-item-active")
 
 
 
