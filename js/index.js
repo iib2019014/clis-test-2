94f33ca2301fde01f292b9e45dea242069ecf0db
manvith <iib2019014@iiita.ac.in>
2022-03-04 12:11:51 +0530

    scroll to shrink fixed

--- js/index.js
+++ js/index.js
@@ -4,54 +4,33 @@ sitebody = document.querySelector(".site-body");
 topbar = document.querySelector(".top-bar-fixed");
 
 navbar_status = true;
-topbar_shrunk = false;
+topbar_shrink = false;
 
-toggler.addEventListener("click", function (event) {
-    if (navbar_status) {
-        if(topbar_shrunk) {
-            navbar.classList.add("navbar-collapse--shrunk");
-            topbar.classList.add("top-bar-adjust--shrunk");
-            sitebody.classList.add("site-body-adjust--shrunk");
-            navbar.classList.remove("navbar-un-collapse--shrunk");
-            topbar.classList.remove("top-bar-un-adjust--shrunk");
-            sitebody.classList.remove("site-body-un-adjust--shrunk");
-        }
+// toggler.addEventListener("click", function (event) {
+//     if (navbar_status) {
+//         topbar.classList.add("top-bar-adjust");
+//         topbar.classList.remove("top-bar-un-adjust");
+//         navbar.classList.add("navbar-collapse");
+//         sitebody.classList.add("site-body-adjust");
+//         navbar.classList.remove("navbar-un-collapse");
+//         sitebody.classList.remove("site-body-un-adjust");
 
-        else {
-            navbar.classList.add("navbar-collapse");
-            topbar.classList.add("top-bar-adjust");
-            sitebody.classList.add("site-body-adjust");
-            navbar.classList.remove("navbar-un-collapse");
-            topbar.classList.remove("top-bar-un-adjust");
-            sitebody.classList.remove("site-body-un-adjust");
-        }
 
-        navbar_status = false;
-    }
+//         navbar_status = false;
+//     }
 
-    else {
-        if(topbar_shrunk) {
-            navbar.classList.add("navbar-un-collapse--shrunk");
-            topbar.classList.add("top-bar-un-adjust--shrunk");
-            sitebody.classList.add("site-body-un-adjust--shrunk");
-            navbar.classList.remove("navbar-collapse--shrunk");
-            topbar.classList.remove("top-bar-adjust--shrunk");
-            sitebody.classList.remove("site-body-adjust--shrunk");
-        }
+//     else {
+//         topbar.classList.add("top-bar-un-adjust");
+//         topbar.classList.remove("top-bar-adjust");
+//         navbar.classList.add("navbar-un-collapse");
+//         sitebody.classList.add("site-body-un-adjust");
+//         navbar.classList.remove("navbar-collapse");
+//         sitebody.classList.remove("site-body-adjust");
 
-        else {
-            navbar.classList.add("navbar-un-collapse");
-            topbar.classList.add("top-bar-un-adjust");
-            sitebody.classList.add("site-body-un-adjust");
-            navbar.classList.remove("navbar-collapse");
-            topbar.classList.remove("top-bar-adjust");
-            sitebody.classList.remove("site-body-adjust");
-        }
 
-        navbar_status = true;
-    }
-
-})
+//         navbar_status = true;
+//     }
+// })
 
 // active_page_util = document.querySelector(".keep-link-active");
 active_page_util = document.querySelectorAll(".keep-link-active");
@@ -141,40 +120,49 @@ syllabus_headers.forEach((syllabus_header) => {
 // scrolling js
 iiita_logo_1 = document.querySelector(".iiita-logo-img-1");
 iiita_header = document.querySelector(".iiita-header");
+iiita_name = document.querySelectorAll(".iiita-name > h4");
+
+// $(':root').css("--font-bree", "'Lobster', cursive");
 
 window.addEventListener('scroll', function (event) {
-    // console.log("window scrolled")
-
     let scrollY = window.scrollY;
     // if not used let, then scrollY is bot changing,
 
-    // console.log(window.scrollY)
-
     if(scrollY >= 325) {
-        // console.log(iiita_logo_1.classList);
+        topbar_shrink = true;
+
         iiita_logo_1.classList.add("iiita-logo-shrink");
         iiita_logo_1.classList.remove("iiita-logo-expand");
 
-        // console.log(iiita_header.classList);
         iiita_header.classList.add("iiita-header-shrink");
         iiita_header.classList.remove("iiita-header-expand");
 
+        iiita_name.forEach((iiita_name_x) => {
+            iiita_name_x.classList.add("iiita-name-shrink");
+            iiita_name_x.classList.remove("iiita-name-expand");
+        })
+
         topbar.classList.add("top-bar-fixed-shrink");
         topbar.classList.remove("top-bar-fixed-expand");
-
-        // document.documentElement.style.setProperty("--topbar-collapse-shrink-height", "10rem");
     }
 
     else {
+        topbar_shrink = false;
+        
         iiita_logo_1.classList.add("iiita-logo-expand");
         iiita_logo_1.classList.remove("iiita-logo-shrink");
 
         iiita_header.classList.add("iiita-header-expand");
         iiita_header.classList.remove("iiita-header-shrink");
 
+        iiita_name.forEach((iiita_name_x) => {
+            iiita_name_x.classList.add("iiita-name-expand");
+            iiita_name_x.classList.remove("iiita-name-shrink");
+        })
+
         topbar.classList.add("top-bar-fixed-expand");
         topbar.classList.remove("top-bar-fixed-shrink");
-
-        // document.documentElement.style.setProperty("--topbar-collapse-shrink-height", "15rem");
     }
+
+    console.log(iiita_name.classList);
 })
